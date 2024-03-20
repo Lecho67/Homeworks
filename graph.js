08c9f0b90d95a39a7bd20c3e9ec00070209bff25
Simon <82274800+Lecho67@users.noreply.github.com>
2024-03-20 15:42:32 -0500

    Creacion
    Arreglando(creo)

---
+++ graph.js
@@ -0,0 +1,30 @@
+class Graph{
+    constructor(){
+        this.nodes=[]
+        this.adjList={}
+    }
+    addNode(node){
+        this.nodes.push(node)
+        this.adjList[node]=[]
+    }
+    addEdge(node1,node2){
+        this.adjList[node1].push(node2)
+        this.adjList[node2].push(node1)
+    }
+    printGraph(){
+        console.log(this.adjList)
+    }
+    searchNode(value){
+        if(!this.nodes.length){
+            return;
+        }
+        return this.nodes.find(item => item === value)
+    }
+    printAdjency(value){
+        if(this.searchNode(value)){
+            console.log(this.adjList[value])
+        }else{
+            return;
+        }
+    }
+}
