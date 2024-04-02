a23689e8b9dcab1f6d9c525b58ad02cbdbe995f4
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-02 15:11:54 -0500

    Terminado
    Pase los cambios de la pc de la uao a mi pc

--- graph.js
+++ graph.js
@@ -28,3 +28,36 @@ class Graph{
         }
     }
 }
+
+let grafo = new Graph();
+
+let ciudad1 = "Barranquilla";
+let ciudad2 = "Cali";
+grafo.addNode(ciudad1);
+grafo.addNode(ciudad2);
+let persona1 = {nombre: "Edwin", edad: 52, ciudad: "Cali"};
+let persona2 = {nombre: "Cuello", edad: 22, ciudad: "Cali"};
+let persona3 = {nombre: "JoseMaria", edad: 21, ciudad: "Barranquilla"};
+let persona4 = {nombre: "Jose", edad: 41, ciudad: "Barranquilla"};
+grafo.addNode(persona1);
+grafo.addNode(persona2);
+grafo.addNode(persona3);
+grafo.addNode(persona4);
+grafo.addEdge(persona1, ciudad1);
+grafo.addEdge(persona2, ciudad1);
+grafo.addEdge(persona3, ciudad2);
+
+function buscarpersonasEnlaciudad(grafo, nombreCiudad){
+    let personasCiudad = [];
+    
+    for(let node of grafo.nodes){
+        if(node.ciudad && node.ciudad === nombreCiudad){
+            personasCiudad.push(node.nombre);
+        }
+    }
+    
+    console.log(`Personas en ${nombreCiudad}: ${personasCiudad.join(",")}`);
+}
+
+buscarpersonasEnlaciudad(grafo, "Cali");
+buscarpersonasEnlaciudad(grafo, "Barranquilla");
