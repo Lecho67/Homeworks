014ea98039b967bc57b8a2fa43328c0cbc511bba
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-23 17:43:45 -0500

    Yap, listo
    Pero me falta cuadrar una cosa en la busqueda.Despues lo hago

---
+++ first-app/src/components/GiftGrid.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+import GifItem from "./GifItem";
+
+function GifGrid({ category }) {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const images = await getGifs(category);
+    setImages(images);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  return (
+    <>
+      <h3>{category}</h3>
+      <h1>Se vino la API</h1>
+      <div className="card-grid">
+          {
+              images.map((image, key) => {
+                  return <GifItem key={key} {...image}></GifItem>
+              })
+          }
+      </div>
+    </>
+  );
+}
+
+export default GifGrid;
