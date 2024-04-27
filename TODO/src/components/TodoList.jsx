b7300a0b08eb892a048901ccb228703c356ddbfb
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-27 02:23:44 -0500

    listo
    ME falta hacer que elimine la tarea.Creo...

---
+++ TODO/src/components/TodoList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TodoItem from "./TodoItem";
+
+function TodoList({todos = []}) {
+  return (
+      <ul>
+          {
+           todos.map( (todo, key) => 
+           {return(<TodoItem key={key} todo={todo}></TodoItem>)})   
+          }
+      </ul>
+  )
+}
+
+export default TodoList
