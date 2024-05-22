01525e5487dd17abfdb4ab885297d3529c987f02
Simon <82274800+Lecho67@users.noreply.github.com>
2024-05-22 11:29:17 -0500

    a

---
+++ CH-REDUZ/src/components/Increment.jsx
@@ -0,0 +1,34 @@
+import {useDispatch, useSelector} from 'react-redux'
+import { useState } from 'react';
+
+export const Increment = () =>{
+
+    const [amount, setAmount] = useState(1);
+    const {counter} = useSelector( state => state.counter);
+    const dispatch = useDispatch();
+
+    const handleIncrement =()=>{
+        dispatch (increment(amount));
+    }
+    const handleDecrement =()=>{
+        dispatch (decrement(amount));
+    }
+    return(
+        <>
+            <h1>App</h1>
+            <hr />
+            <span> Counter is : {counter}</span>
+            <input type="number" onChange={(x) => setAmount(parseInt(x.target.value))}/>
+            <button
+                className="btn btn - primary"
+                onClick={handleIncrement}>
+            +
+            </button>
+            <button
+                className="btn btn - primary"
+                onClick={handleDecrement}>
+            -
+            </button>
+        </>
+    )
+}
