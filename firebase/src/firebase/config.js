57d6054f62e724e3e217534a7a550e81870b3b20
Simon <82274800+Lecho67@users.noreply.github.com>
2024-06-01 18:23:40 -0500

    Listo

--- firebase/src/firebase/config.js
+++ firebase/src/firebase/config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
-import {getStorage} from 'firebase/storage';
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,10 +15,5 @@ const firebaseConfig = {
   messagingSenderId: "913088556636",
   appId: "1:913088556636:web:ae0b1cec5137cd970ec099"
 };
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth()
-const firebaseStorage = getStorage(firebaseApp)
-
-// Initialize Firebase
-
-export {firebaseApp, auth, firebaseStorage}
+initializeApp(firebaseConfig);
+export const auth = getAuth();
